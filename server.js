31e3f88aeab1b01fee12417b6d069c8b142a29b5
Henrique <henrihiga@gmail.com>
2025-09-25 23:36:56 -0300

    change ip

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fetch = global.fetch || require('node-fetch'); // node 18+ tem fetch global
 const app = express();
 
-const BACKEND_URL = process.env.BACKEND_URL || 'http://172.31.18.11:25000';
+const BACKEND_URL = process.env.BACKEND_URL || 'http://10.0.2.234:25000';
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
